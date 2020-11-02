e7175c764ce58294c5b15bb3524b9bf85382a426
Enrique Jimenez <enrique.jmz37@gmail.com>
2020-11-01 22:26:01 -0600

    clase16

---
+++ funciones.py
@@ -0,0 +1,26 @@
+# def imprimir_mensaje():
+#     print("Mensaje especial: ")
+#     print("¡Estoy aprendiendo a usar funciones!")
+
+
+# imprimir_mensaje()
+# imprimir_mensaje()
+# imprimir_mensaje()
+
+
+def conversacion(mensaje):
+    print("Hola")
+    print("Cómo estás")
+    print(mensaje)
+    print("Adios")
+
+
+opcion = int(input("Eliege una opción (1, 2, 3): "))
+if opcion == 1:
+    conversacion("Elegiste la opción 1")
+elif opcion == 2:
+    conversacion("Elegiste la opción 2")
+elif opcion == 3:
+    conversacion("Elegiste la opción 3")
+else:
+    print("Escribe la opción correcta")
